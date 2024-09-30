8afa0010a0fb7cb11f9a8a03e60776d0c4740d9f
CarlosLonghi <carloslonghi.cl@gmail.com>
2024-09-30 12:25:12 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="h-screen bg-zinc-900 flex flex-col gap-12 items-center justify-center">
+      <h1 className="text-5xl text-zinc-100">React + TypeScript + Vite</h1>
+      <h2 className="text-4xl text-zinc-200">Tailwind CSS</h2>
+    </main>
+  )
+}
+
+export default App
