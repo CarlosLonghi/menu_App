8defea984d6a6a0a5926d988971f8aaf4951f8fc
CarlosLonghi <carloslonghi.cl@gmail.com>
2024-10-02 01:22:41 -0300

    style: add style card, and fetch foodData

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import { Card } from "./components/card";
-import { FoodData } from "./interface/FoodData";
+import { useFoodData } from "./hooks/useFoodData";
 
 function App() {
-  const data: FoodData[] = [];
+  const { data } = useFoodData();
 
   return (
     <main className="max-w-screen-lg mx-auto px-8 py-10">
-      <h1 className="text-4xl text-zinc-100">Cardápio</h1>
+      <h1 className="text-2xl text-zinc-100 text-center font-bold mb-10 underline underline-offset-4 decoration-zinc-100 md:text-4xl">Cardápio</h1>
       
-      <div className="grid-cols-3">
-        {data.map((item) => (
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+        { data ? data.map((item) => (
           <Card 
+            key={item.id}
             title={item.title} 
             image={item.image} 
             price={item.price} 
           />
-        ))}
+        )): 
+          <h2 className="text-zinc-100">Carregando...</h2>
+        }
       </div>
     </main>
   )
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,10 +6,15 @@ interface CardProps {
 
 export function Card({title, image, price} : CardProps) {
   return (
-    <div className="bg-white rounded-lg shadow-md p-4">
-      <img src={image} alt=""/>
-      <h2>{title}</h2>
-      <p><b>Preço:{price}</b></p>
+    <div className="bg-gradient-to-tl to-zinc-700/80 from-zinc-600/80 rounded-lg ring-2 ring-zinc-500 shadow-zinc-950 shadow-lg col-span-1 overflow-hidden">
+      <img className="w-full h-56 object-cover" src={image} alt={title}/>
+      <div className="p-4 space-y-2">
+        <h2 className="text-zinc-200 capitalize text-base font-semibold md:text-lg">{title}</h2>
+        <p className="text-zinc-300">
+          <b className="font-semibold">Preço: </b>
+          <b className="text-emerald-500">R${price.toFixed(2).replace('.', ',')}</b>
+        </p>
+      </div>
     </div>
   )
 }
